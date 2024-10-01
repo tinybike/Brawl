bd37f92a43dabf81dc6c526b42abea28c9a289a2
Jack Peterson <jack@tinybike.net>
2024-10-01 15:48:55 -0700

    Testing noesis stuff

--- Brawl/Mods/Brawl/ScriptExtender/Lua/BootstrapClient.lua
+++ Brawl/Mods/Brawl/ScriptExtender/Lua/BootstrapClient.lua
@@ -1,13 +1,20 @@
-local shiftPressed = false
-local spacePressed = false
+local isShiftPressed = false
+local isSpacePressed = false
 
+-- NB: key rebindings
 Ext.Events.KeyInput:Subscribe(function (e)
     if e.Key == "LSHIFT" or e.Key == "RSHIFT" then
-        shiftPressed = e.Event == "KeyDown"
+        isShiftPressed = e.Event == "KeyDown"
     elseif e.Key == "SPACE" then
-        spacePressed = e.Event == "KeyDown"
+        isSpacePressed = e.Event == "KeyDown"
     end
-    if shiftPressed and spacePressed then
+    if isShiftPressed and isSpacePressed then
         Ext.ClientNet.PostMessageToServer("Toggle_TurnBased", "")
     end
+end)
+
+Ext.Events.SessionLoaded:Subscribe(function ()
+    Ext.UI.GetRoot():Child(1):Child(1):Child(3):VisualChild(1):Child(5):Child(5):Child(7):Subscribe("GotMouseCapture", function ()
+        Ext.ClientNet.PostMessageToServer("Toggle_TurnBased", "")
+    end)
 end)
