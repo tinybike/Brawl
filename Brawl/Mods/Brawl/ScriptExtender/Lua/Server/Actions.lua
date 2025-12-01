052faf76e5b9176f34dc085d403ee80178fcd26d
Jack Peterson <jack@tinybike.net>
2025-11-30 21:00:19 -0800

    Targeting fix

--- Brawl/Mods/Brawl/ScriptExtender/Lua/Server/Actions.lua
+++ Brawl/Mods/Brawl/ScriptExtender/Lua/Server/Actions.lua
@@ -371,7 +371,6 @@ end
 return {
     getActionInProgress = getActionInProgress,
     getActionInProgressByName = getActionInProgressByName,
-    registerActionInProgress = registerActionInProgress,
     removeActionInProgress = removeActionInProgress,
     removeActionsInProgress = removeActionsInProgress,
     handleExtraAttacks = handleExtraAttacks,
